53b990a2061527d9f0712af2c938d1c10e713a5d
bodzio2k <bodzio2k@live.com>
2018-11-27 19:59:15 +0100

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Krzysztof Podolak on 27/11/2018.
+//  Copyright © 2018 Krzysztof Podolak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
