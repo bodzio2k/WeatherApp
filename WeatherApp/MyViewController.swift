301a3a0b06c9f32fc1aa57038258da1c4851442a
bodzio2k <bodzio2k@live.com>
2019-09-07 17:33:53 +0200

    Removed unwanted workspace

---
+++ WeatherApp/MyViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.swift
+//  WeatherApp
+//
+//  Created by Krzysztof Podolak on 07/08/2019.
+//  Copyright © 2019 Krzysztof Podolak. All rights reserved.
+//
+
+import UIKit
+
+class MyScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
